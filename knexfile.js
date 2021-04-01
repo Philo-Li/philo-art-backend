edbbd21ffd5f6533c73955f25ae9db5459d7d774
Philo <maerd.lll.p@gmail.com>
2021-04-01 13:59:51 +0800

    update config pool

--- knexfile.js
+++ knexfile.js
@@ -25,4 +25,14 @@ if (process.env.NODE_ENV === 'development') {
 module.exports = {
   client: 'pg',
   connection,
+  pool: {
+    min: 2,
+    max: 8,
+    createTimeoutMillis: 3000,
+    acquireTimeoutMillis: 30000,
+    idleTimeoutMillis: 30000,
+    reapIntervalMillis: 1000,
+    createRetryIntervalMillis: 100,
+    propagateCreateError: false, // <- default is true, set to false
+  },
 };
--- src/config.js
+++ src/config.js
@@ -31,6 +31,16 @@ export default {
   database: {
     client: 'pg',
     connection,
+    pool: {
+      min: 2,
+      max: 8,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false, // <- default is true, set to false
+    },
   },
   imagga: {
     apiUrl: process.env.IMAGGA_API_URL,
