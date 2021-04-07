cf6833350a29585f1bd03ad5ec5a740a0d8b55a2
Philo <maerd.lll.p@gmail.com>
2021-04-07 22:44:48 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -26,16 +26,6 @@ export default {
   database: {
     client: 'pg',
     connection,
-    pool: {
-      min: 2,
-      max: 6,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false, // <- default is true, set to false
-    },
   },
   imagga: {
     apiUrl: process.env.IMAGGA_API_URL,
