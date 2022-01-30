fb2c5fafbc9825daf64a2f0db9bf5baf71d633d6
Philo <maerd.lll.p@gmail.com>
2022-01-30 23:46:05 +0800

    update pg connection config

--- knexfile.js
+++ knexfile.js
@@ -2,23 +2,18 @@ require('dotenv').config();
 
 let connection;
 
-if (process.env.NODE_ENV === 'development') {
-  connection = process.env.DEV_PG_CONNECTION_STRING;
-} else if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
+  
+} else if (process.env.NODE_ENV == 'development') {
   connection = {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME_TEST,
-  };
-} else if (process.env.NODE_ENV == 'stage') {
-  connection = {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME,
+    host: process.env.AWS_PG_HOST,
+    port: process.env.AWS_PG_PORT,
+    user: process.env.AWS_PG_USER,
+    password: process.env.AWS_PG_PASSWORD,
+    database: process.env.AWS_PG_DBNAME,
+    migrations: {
+      tableName: 'migrations'
+    }
   };
 } else if (process.env.NODE_ENV === 'production') {
   connection = {
--- src/config.js
+++ src/config.js
@@ -5,26 +5,15 @@ dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 let connection;
 
-if (process.env.NODE_ENV === 'development') {
-  connection = process.env.DEV_PG_CONNECTION_STRING;
-} else if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
+  
+} else if (process.env.NODE_ENV == 'development') {
   connection = {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME_TEST,
-    migrations: {
-      tableName: 'migrations'
-    }
-  };
-} else if (process.env.NODE_ENV == 'stage') {
-  connection = {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME,
+    host: process.env.AWS_PG_HOST,
+    port: process.env.AWS_PG_PORT,
+    user: process.env.AWS_PG_USER,
+    password: process.env.AWS_PG_PASSWORD,
+    database: process.env.AWS_PG_DBNAME,
     migrations: {
       tableName: 'migrations'
     }
