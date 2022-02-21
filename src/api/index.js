879be20a3c458c4e57df88c9b63d2ecd88a912a0
Philo <maerd.lll.p@gmail.com>
2022-02-21 17:35:36 +0800

    remove api: upload img to server

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,6 @@ import burst from './burst';
 import picography from './picography';
 import pixabay from './pixabay';
 import pexels from './pexels';
-import uploadSingleFile from './upload-single-file';
 import generateUploadURL from './s3';
 
 const router = new Router();
@@ -22,8 +21,6 @@ router.use('/pixabay', pixabay.routes());
 
 // web scrapping fobidden
 router.use('/pexels', pexels.routes());
-
-router.use('/upload', uploadSingleFile.routes());
 
 router.get('/s3Url/:userId', async (res) => {
   const { userId } = res.params;
