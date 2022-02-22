ef9b26b64764c1fca151ce30e95ccc588197f7bd
Philo <maerd.lll.p@gmail.com>
2022-02-22 18:02:41 +0800

    remove s3url router

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,6 @@ import burst from './burst';
 import picography from './picography';
 import pixabay from './pixabay';
 import pexels from './pexels';
-import generateUploadURL from './s3';
 
 const router = new Router();
 
@@ -21,13 +20,5 @@ router.use('/pixabay', pixabay.routes());
 
 // web scrapping fobidden
 router.use('/pexels', pexels.routes());
-
-router.get('/s3Url/:userId', async (res) => {
-  const { userId } = res.params;
-  const url = await generateUploadURL(userId);
-  res.body = {
-    url,
-  };
-});
 
 export default router;
